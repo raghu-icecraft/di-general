adb78e73f324f67b3cf5edff8227599608be2a22
raghu.icecraft <raghu.icecraft@gmail.com>
2017-11-24 13:56:50 +0530

    Adjusted test files to src/test/java location. Now Maven life cycle works, checked clean, compile and test.
    Also both the individual test files having main method can be executed separately.

---
+++ src/test/java/individual/tests/MyLegacyTest.java
@@ -0,0 +1,12 @@
+package individual.tests;
+
+import com.journaldev.java.legacy.MyApplication;
+
+public class MyLegacyTest {
+
+	public static void main(String[] args) {
+		MyApplication app = new MyApplication();
+		app.processMessages("Hi Pankaj", "pankaj@abc.com");
+	}
+
+}
